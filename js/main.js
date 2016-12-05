764382f562aab5355af1dfeae8f2bbd04ee10a02
enbo <northlight2015@gmail.com>
2016-12-05 20:36:24 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+(function() {
+    'use strict';
+    
+    moment.locale('fi');
+    
+    // var weekdays = ['maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai', 'sunnuntai'];
+    var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    
+    var selectedDate = moment(new Date());
+        
+    var contentElem = document.getElementById('main-content'),
+    
+        source = document.getElementById('main-template').innerHTML,
+        template = doT.template(source),
+        loadingIcon = '<div class="loading-icon"><i class="fa fa-circle-o-notch fa-spin"></i></div>',
+        
+        apiUrl = 'http://tutmenuapi.ncphdpxnki.us-west-2.elasticbeanstalk.com/';
+        
+    updateMenus(selectedDate);
+    
+    document.getElementById('next-day').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        selectedDate.add(1, 'day');
+        
+        updateMenus(selectedDate);
+    });
+    
+    document.getElementById('prev-day').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        selectedDate.subtract(1, 'day');
+        
+        updateMenus(selectedDate);
+    });
+        
+    
+    function updateMenus(date) {
+        contentElem.innerHTML = loadingIcon;
+        
+        var dateString = date.format('YYYY-MM-DD'),
+            requestUrl = apiUrl + dateString;
+            
+        var req = new XMLHttpRequest();
+        
+        req.addEventListener('load', function() {
+            var context = JSON.parse(this.responseText);
+            context.date = weekdays[date.weekday()] + ' ' + date.format('D.M.');
+            
+            contentElem.innerHTML = template(context);
+        });
+        
+        req.open('get', requestUrl, true);
+        req.send();
+    }
+})();
